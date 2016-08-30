94ffa5af65136268e59d135d121e83b1c30302b7
Jon Dahl <jon@mux.com>
2016-08-30 13:49:31 -0700

    annotate

--- starter.rb
+++ starter.rb
@@ -6,13 +6,16 @@ values["te"] = Hash.new
 values["wr"] = Hash.new(0)
 values["rb"] = Hash.new(0)
 
+
+# TODO: simulate this draft http://www.fleaflicker.com/nfl/leagues/158052/drafts
+
 values["qb"][1] = 63 # Newton, Wilson
 values["qb"][2] = 63
 values["qb"][3] = 63
 values["qb"][4] = 63 # Wilson, sometimes Newton
 values["qb"][5] = 43 # Brady
 values["qb"][6] = 43 #
-values["qb"][7] = 43 # Brady still available here
+values["qb"][7] = 43 # Brady gone sometimes!
 values["qb"][8] = 10 # Brady here 6/7. Otherwise, down to next tier.
 values["qb"][9] = 7 # OCCASIONALLY Brady
 values["qb"][10] = 7
@@ -21,19 +24,19 @@ values["rb"][1] = 62 # Lamar Miller here 4/6.
 values["rb"][2] = 53 # Peterson
 values["rb"][3] = 20
 values["rb"][4] = 18
-values["rb"][5] = 18
+values["rb"][5] = 18 # Woodhead occasionally (rarely). Langford, Gore, Foster, Rawls
 values["rb"][6] = 1
 values["rb"][7] = -10 # Sims 0. Otherwise, -10
 values["rb"][8] = -10
 values["rb"][9] = -15
 values["rb"][10] = -15
 
-values["wr"][1] = 85 # Robinson
-values["wr"][2] = 82 # Marshall
-values["wr"][3] = 51 # Cobb 4/7. Edelman, Watkins, Landry. usually. Worst draft: http://www.fleaflicker.com/nfl/leagues/124678/drafts.
+values["wr"][1] = 85 # Robinson. BAD DRAFT: http://www.fleaflicker.com/nfl/leagues/158052/drafts - would miss out on most targets.
+values["wr"][2] = 82 # Robinson or Marshall, or drop to Gronk/Miller? NOT DEZ (Romo injury.
+values["wr"][3] = 51 # Cobb 6/10. Watkins, Landry, Edelman. usually. Worst draft: http://www.fleaflicker.com/nfl/leagues/124678/drafts.
 values["wr"][4] = 46 # Decker or better
-values["wr"][5] = 38 # Fitzgerald 5/7
-values["wr"][6] = 20 # Matthews, Sanders sometimes. Crabtree, Hurns, Brown.
+values["wr"][5] = 38 # Moncrief once. Fitzgerald 5/10
+values["wr"][6] = 20 # Matthews, Sanders sometimes. Crabtree, Hurns, Brown. Gordon always.
 values["wr"][7] = 15 # Josh Gordon sometimes. Not much otherwise.
 values["wr"][8] = -1 # Gordon rarely. Sterling Shepherd
 values["wr"][9] = -5 #
